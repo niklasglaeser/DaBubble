fd747cd3694be256c5d20cab6d7f6b22a24a06c7
Safa325 <shamarisafa@gmail.com>
2024-08-08 16:42:06 +0200

    %*

--- DaBubble/src/app/app.component.ts
+++ DaBubble/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LandingPageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
