ba85c6214a3f7d966f3769bff19baad34b62d366
Marco Engelhardt <sirmarco@reussnet.ch>
2024-08-13 21:18:49 +0200

    rebuild onSnapshot + edit channel

--- DaBubble/src/app/models/channel.service.ts
+++ DaBubble/src/app/models/channel.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import {
   collection,
-  collectionData,
   Firestore,
   doc,
   addDoc,
+  updateDoc,
+  onSnapshot,
 } from '@angular/fire/firestore';
 import { Channel } from './channel.class';
 
@@ -13,10 +14,42 @@ import { Channel } from './channel.class';
 })
 export class ChannelService {
   private firestore = inject(Firestore);
-  channels$;
+  channels: Channel[] = [];
+
+  /*onSnapshot variablen*/
+  unsubList;
+  unsubSingle;
 
   constructor() {
-    this.channels$ = collectionData(this.getChannelsRef());
+    this.unsubList = this.channelsList();
+
+    this.unsubSingle = onSnapshot(
+      this.getSingleChannel('channels', 'dfsdf'),
+      (element) => {}
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.unsubList(); // snapshot unsubscribe
+    this.unsubSingle(); // snapshot unsubscribe
+  }
+
+  channelsList() {
+    return onSnapshot(this.getChannelsRef(), (list) => {
+      this.channels = [];
+      list.forEach((element) => {
+        this.channels.push(this.setChannelObject(element.data(), element.id));
+      });
+    });
+  }
+
+  setChannelObject(obj: any, id: string): Channel {
+    return {
+      id: id,
+      name: obj.name,
+      description: obj.description || '',
+      creator: obj.creator,
+    };
   }
 
   getSingleChannel(colId: string, docId: string) {
@@ -37,6 +70,20 @@ export class ChannelService {
       console.log('Channel created with ID:' + docRef.id);
     } catch (error) {
       console.error('error adding channel' + error);
+    }
+  }
+
+  async updateChannel(channelId: string, channel: Channel) {
+    try {
+      const channelDocRef = this.getSingleChannel('channels', channelId);
+      await updateDoc(channelDocRef, {
+        name: channel.name,
+        description: channel.description,
+        creator: channel.creator,
+      });
+      console.log('Channel updated with ID: ', channelId);
+    } catch (e) {
+      console.error('Error updating document: ', e);
     }
   }
 }
