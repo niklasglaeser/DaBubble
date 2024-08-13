281e5c66bf0f523513b1a252e615f275f123bfef
Safa325 <shamarisafa@gmail.com>
2024-08-13 16:26:32 +0200

    firebase

--- DaBubble/src/app/landing-page/login/login.component.ts
+++ DaBubble/src/app/landing-page/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import { LandingPageComponent } from '../landing-page.component';
+import { AuthService } from '../../services/lp-services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ import { LandingPageComponent } from '../landing-page.component';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  authService = inject(AuthService)
   isSubmited: boolean = false
 
   registerForm = this.fb.group({
