6aaa1408b39a082e82ec6286e6314dbcf5296fe1
Safa325 <shamarisafa@gmail.com>
2024-08-13 16:45:59 +0200

    %*

--- DaBubble/src/app/app.config.ts
+++ DaBubble/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
@@ -19,8 +20,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimationsAsync(), 
+    provideHttpClient(), 
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(()=>getAuth())
-    
   ]
 };
--- DaBubble/src/app/services/lp-services/auth.service.ts
+++ DaBubble/src/app/services/lp-services/auth.service.ts
@@ -16,3 +16,5 @@ export class AuthService{
         return from(promise)
     }
 }
+
+
