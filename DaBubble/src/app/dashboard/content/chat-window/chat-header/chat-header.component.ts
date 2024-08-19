9e4e412538921178d01340b32e6f0f463530ebaf
Marco Engelhardt <sirmarco@reussnet.ch>
2024-08-19 22:18:48 +0200

    add dialog edit channel

--- DaBubble/src/app/dashboard/content/chat-window/chat-header/chat-header.component.ts
+++ DaBubble/src/app/dashboard/content/chat-window/chat-header/chat-header.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Channel } from '../../../../models/channel.class';
 import { UserLogged } from '../../../../models/user-logged.model';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogChannelEditComponent } from '../../../../dialog/dialog-channel-edit/dialog-channel-edit.component';
 
 @Component({
   selector: 'app-chat-header',
@@ -12,4 +14,22 @@ import { UserLogged } from '../../../../models/user-logged.model';
 export class ChatHeaderComponent {
   @Input() channel: Channel | null = null;
   @Input() members: UserLogged[] = [];
+
+  constructor(public dialog: MatDialog) {}
+
+  openEditChannel(): void {
+    if (this.channel && this.channel.id) {
+      const dialogRef = this.dialog.open(DialogChannelEditComponent, {
+        data: { channelId: this.channel.id },
+      });
+
+      dialogRef.afterClosed().subscribe((result) => {
+        if (result) {
+          console.log('Dialog result:', result);
+        }
+      });
+    } else {
+      console.error('No channel ID');
+    }
+  }
 }
