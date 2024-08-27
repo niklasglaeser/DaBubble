965bc2d80e5936e63742112b6971ae88626b4643
Safa325 <shamarisafa@gmail.com>
2024-08-27 16:09:48 +0200

    authGuard

--- DaBubble/src/app/app.routes.ts
+++ DaBubble/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent},
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]  },
   { path: ':action', component: LandingPageComponent},
  
 ];
---
+++ DaBubble/src/app/services/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { AuthService } from './lp-services/auth.service';
+import { tap, switchMap, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean> {
+    
+
+    if (!localStorage.getItem('uid')) {
+      this.router.navigate(['']); 
+      return of(false); 
+    }
+    return of(true); 
+  }
+}
